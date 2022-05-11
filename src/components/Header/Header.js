b5e919eed0a5f19e9bc215034198803d2b332e8f
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2022-05-11 19:38:17 +0200

    creating header component, assets and home page in addition to, routing pages updating..

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import logo from '../../assets/images/logo.jpg';
+import menu from '../../assets/images/hamburger.png';
+import close from '../../assets/images/close.png';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+  const Links = [
+    {name: "Home", link: "/"},
+    {name: "About", link: "/about"},
+    {name: "Program", link: "/program"},
+    {name: "Booking", link: "/booking"},
+    {name: "Things to do", link: "/things_to_do"},
+    {name: "Contact", link: "/contact"},
+  ];
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header id="header">
+      <div className='shadow-md w-full fixed top-0 left-0 z-20'>
+        <nav className='md:flex justify-between items-center bg-white py-4 md:px-10 px-7'>
+          <div>
+            <img className='w-20 h-auto' src={logo} alt="logo_image" />
+          </div>
+          <div
+            onClick={() => setOpen(!open)}
+            className='text-3xl absolute right-8 top-12 cursor-pointer md:hidden'
+          >
+            {
+              open
+              ? <img src={close} alt="menu_icon" />
+              : <img src={menu} alt="close_icon" />
+            }
+          </div>
+          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px] md:opacity-100 opacity-0'}`}>
+            {
+              Links.map(link => (
+                <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                  <NavLink to={ link.link } className='text-gray-800 hover:text-gray-400 duration-500'>{ link.name }</NavLink>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/container/App.js
+++ src/container/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/program" element={<Program />} /> */}
+          {/* <Route path="/booking" element={<Booking />} /> */}
+          {/* <Route path="/things_to_do" element={<ThingsToDo />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
