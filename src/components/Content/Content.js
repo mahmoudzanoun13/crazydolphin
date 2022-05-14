4bd47c7923ff105cae52169ad4de979fe4f6e3a7
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2022-05-14 20:21:26 +0200

    fixing sizes of the image fixing..

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,7 +10,7 @@ export const Content = () => {
     <div id="content" className="bg-gray-800 relative mt-28">
       <div className="grid md:grid-cols-2">
         <div className="my-20 md:my-64 md:ml-40">
-          <img className="px-2 md:pl-0" src={beachIcon} alt="beach_icons" srcSet={`${beachIconOne} 45w, ${beachIconTwo} 40w, ${beachIconThree} 38w`}/>
+          <img className="px-2 md:pl-0" src={beachIcon} alt="beach_icons" srcSet={`${beachIconOne} 45w, ${beachIconTwo} 40w, ${beachIconThree} 38w`} sizes="(max-width: 45px), (max-width: 40px), (max-width: 38px), 38px"/>
           <h3 className="text-yellow-500 px-4 md:pl-0 my-2 text-5xl">Hello</h3>
           <div className="container mx-auto px-4 md:pl-0"><hr className="w-full h-1 text-white"/></div>
           <h4 className="text-white px-4 md:pl-0 mt-20 mb-3 text-2xl">Welcome!</h4>
