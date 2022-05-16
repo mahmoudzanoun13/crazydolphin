c5b8d274364820910ff3dfc340a4567f1c5f8152
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2022-05-16 06:23:38 +0200

    changing color of text inside the button for more accessibility fixing contrast ratio

--- src/components/WhatToDo/WhatToDo.js
+++ src/components/WhatToDo/WhatToDo.js
@@ -17,7 +17,7 @@ export const WhatToDo = () => {
             alt="diving_icon"
           />
           <h4 className="mt-12 text-4xl tracking-wider font-medium text-white text-center">FIND THE UNDERWATER TREASURES OF THE RED SEA</h4>
-          <button className="relative left-1/2 -translate-x-1/2 my-12 bg-yellow-500 rounded-full p-5 hover:bg-white transform hover:-translate-y-0.5 focus:outline-none duration-500"><NavLink to="/things_to_do" className="p-5 text-white text-xl font-medium hover:text-yellow-500 duration-500">WHAT TO DO</NavLink></button>
+          <button className="relative left-1/2 -translate-x-1/2 my-12 bg-yellow-500 rounded-full p-5 hover:bg-white transform hover:-translate-y-0.5 focus:outline-none duration-500"><NavLink to="/things_to_do" className="p-5 text-gray-800 text-xl font-medium hover:text-yellow-500 duration-500">WHAT TO DO</NavLink></button>
         </div>
       </div>
     </div>
